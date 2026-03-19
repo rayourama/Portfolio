bd93ce9aa409b4fe3ef10fad3f1fadd2ec824f63
Ryan Ramassamy <rayourama@hotmail.com>
2026-03-19 17:15:26 +0100

    suppression CV

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,13 +7,6 @@ export default function Header() {
       <p className="mt-4 text-lg text-slate-600">
         Développeur Logiciel / Web
       </p>
-      <a
-  href="/CV.pdf"
-  download={"CV_Ryan_Ramassamy.pdf"}
-  className="fixed bottom-4 left-4 bg-blue-600 text-white px-4 py-2 rounded-full shadow-lg hover:bg-blue-700 transition"
->
-  Mon CV
-</a>
     </header>
     
   )
