cd34328c984f7dd17cdcd983809f20a38a526a85
Ryan Ramassamy <rayourama@hotmail.com>
2026-03-19 19:32:30 +0100

    fix problem with icon

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+import icon from './assets/icon.png'
+
+const link = document.createElement('link')
+link.rel = 'icon'
+link.href = icon
+document.head.appendChild(link)
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
